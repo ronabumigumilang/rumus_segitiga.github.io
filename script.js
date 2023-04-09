1427641d77c44d61b6e163c9a237cb579358bf65
ronabumigumilang <ronabumig@gmail.com>
2023-04-09 10:09:58 +0700

    penambahan butoon reset

--- script.js
+++ script.js
@@ -2,9 +2,22 @@ const alas = document.getElementById('alas')
 const tinggi = document.getElementById('tinggi')
 const hitung = document.getElementById('hitung')
 const output = document.getElementById('output')
+let resetAll = document.querySelector('#resetAllInput')
+let inputs = document.querySelectorAll('input')
 hitung.addEventListener('click', function() {
     let a = alas.value
     let t = tinggi.value
     let l = 0.5*a*t
     output.innerHTML = `Luas segitiga dari alas ${a} cm dan tinggi ${t} cm adalah ${l} cm2`
+})
+
+function reset() {
+    output.innerHTML = '';
+}
+
+resetAll.addEventListener('click', () => {
+    // inputs.forEach((input) => input.value = '')
+    inputs.forEach(function(input) {
+        input.value = ''
+    })
 })
